021458c728f72228501a0d53bb0d004bf586e42d
alejmira <alejandro.mirandacortes@gmail.com>
2022-05-20 16:08:50 +0200

    Añadido el código para hacer el muestreo y los archivos del muestreo.

--- BICIMAD.py
+++ BICIMAD.py
@@ -11,6 +11,7 @@ from pyspark import SparkContext
 import datetime
 import sys
 import matplotlib.pyplot as plt
+import random
 
 
 def mapper(line):
@@ -23,7 +24,9 @@ def mapper(line):
 def main():
     with SparkContext() as sc:
         files = ["20"+str(i)+"_movements.json" for i in [1908, 1909, 1910, 1911, 1912, 2001, 2002, 2003, 2004, 2005, 2006, 2007]]
-        rdd = sc.textFile(",".join(files)).map(mapper)
+        muestreo = sc.textFile(",".join(files)).filter(lambda x: random.randint(0, 100) < 1)
+        muestreo.saveAsTextFile("Muestreo")
+        rdd = muestreo.map(mapper)
         cuenta_usos = dict(rdd.countByKey().items())
         cuenta_tipo_usuario_porcentaje = sorted(rdd.map(lambda x: (x[0], [1*(i==x[1]-1) for i in range(3)])).reduceByKey(lambda x, y: [x[i]+y[i] for i in range(3)]).map(lambda x: (x[0], [x[1][i]/cuenta_usos[x[0]] for i in range(3)])).collect())
         media_tiempo = sorted(rdd.map(lambda x: (x[0], x[2])).reduceByKey(lambda x, y: x+y).map(lambda x: (x[0], x[1]/cuenta_usos[x[0]])).collect())
